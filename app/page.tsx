b4ed16c0ef5aeb60207512693912413c81a848b9
Harry Yuhao Han <s2162783@ed.ac.uk>
2024-01-13 22:03:43 +0000

    image fix

--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,20 @@ export default function Home() {
     <main>
       <div className="flex items-center pt-20">
         <Image
-          className="hidden lg:block pl-20"
+          className="hidden lg:block pl-20 w-1/2"
           src={background}
           alt="background"
-          width={600}
-          height={600}
+          width={700}
+          height={700}
         />
         <div className="px-12 lg:px-6 align-center lg:flex-row">
-          <h1 className="text-center text-5xl font-bold leading-[4rem]">
+          <h1 className="text-center text-7xl font-bold leading-[5rem]">
             Haec Data Poena Diu Discentibus
           </h1>
-          <h2 className="text-center text-xl font-medium leading-[2rem]">
+          <h2 className="text-center text-2xl font-medium leading-[3rem]">
             This penalty is only given to those who is learning more.
             <br />
-            <span className="text-base">
+            <span className="text-xl">
               &mdash; <i>The Antiquary</i>, W. Scott, Edinburgh, 1816 </span>
           </h2>
         </div>
