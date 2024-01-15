e0d33b7dece5f1c1fec5adee75fa3b38ddc357db
Harry Yuhao Han <s2162783@ed.ac.uk>
2024-01-15 10:14:24 +0000

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -12,18 +13,17 @@ export const metadata: Metadata = {
     default: "Harry's Profile",
   },
   description: "Harry's Personal Profile and Portfolio Website",
-  metadataBase: new URL('https://yhhan.net'),
+  metadataBase: new URL("https://yhhan.net"),
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={`${monserrat.className}`}>
-        <>
-          <Navbar />
-          <div className="pt-[5vh] min-h-[85vh] flex items-start justify-start">{children}</div>
-          <Footer />
-        </>
+        <Navbar />
+        <div className="pt-[5vh] min-h-[100vh] pb-[5vh] flex items-start justify-start">{children}</div>
+        <Footer />
+        <Analytics />
       </body>
     </html>
   );
