54eff98cbf91404c400687f8937ae26df49c709e
Harry Yuhao Han <s2162783@ed.ac.uk>
2024-01-21 22:06:41 +0000

    update

--- app/ui/TextUnderLineAnimation.tsx
+++ app/ui/TextUnderLineAnimation.tsx
@@ -1,10 +1,12 @@
 "use client";
 import React from "react";
 
+import "./local.css";
+
 const TextUnderLineAnimation = ({
   text,
   className,
-  underlineWidth, 
+  underlineWidth,
   duration,
 }: {
   text: string;
@@ -12,15 +14,12 @@ const TextUnderLineAnimation = ({
   underlineWidth?: string;
   duration?: string;
 }) => {
-  underlineWidth = underlineWidth || "2.4px";
+  underlineWidth = underlineWidth || "1.4px";
   return (
-    <span className={`group relative ${className}`}>
+    <span
+      className={`${className} [background-image:linear-gradient(rgb(0,0,0),rgb(0,0,0))] [background-size:0_2px] [background-position:0%_100%] [background-repeat:no-repeat] [transition:background-size_0.2s_ease] hover:[background-size:100%_2px]`}
+    >
       {text}
-      <span
-        className={`inline-block bg-black absolute left-0 h-[2.4px] -bottom-0.5 w-0 group-hover:w-full transition-[width] ease duration-500`}
-      >
-        &nbsp;
-      </span>
     </span>
   );
 };
