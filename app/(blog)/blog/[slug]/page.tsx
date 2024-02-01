89ba5e3355511829e17572dae22142090299bda7
Harry Yuhao Han <s2162783@ed.ac.uk>
2024-02-01 10:30:35 +0000

    load to ssc

--- app/(blog)/blog/[slug]/page.tsx
+++ app/(blog)/blog/[slug]/page.tsx
@@ -3,18 +3,9 @@ import Markdown from "markdown-to-jsx";
 import "@/app/(blog)/blog.css";
 import Code from "@/app/(blog)/components/Code";
 
-import blogMap from "@/app/(blog)/blog/[slug]/blogs"
+import { blogMap }  from "@/app/(blog)/blog/page"
 
 export default function Page({ params }: { params: { slug: string } }) {
-  const fs = require("fs");
-  const filePath = `content/${params.slug}`;
-  var str;
-  try {
-    str = fs.readFileSync(filePath, "utf8");
-  } catch (err) {
-    str =
-      "# 404: Page Not Found\n\nSorry, but the page you were trying to view does not exist.\n\nIt looks like this was the result of either:\n\n- a mistyped address\n- an out-of-date link\n\n[Go back to home page](/blog)";
-  }
   return (
     <div className="flex justify-center w-full">
       <div className="text-left mx-[10vw]">
@@ -33,7 +24,7 @@ export default function Page({ params }: { params: { slug: string } }) {
             },
           }}
         >
-          {blogMap.get(params.slug).content}
+          {blogMap.get(params.slug).content || "404"}
         </Markdown>
       </div>
     </div>
--- app/(blog)/blog/page.tsx
+++ app/(blog)/blog/page.tsx
@@ -1,4 +1,22 @@
 import React from "react";
+import fs from "fs";
+
+const blogMap = new Map<string, any>();
+
+const filePath = `content/os2024.md`;
+var str;
+try {
+  str = fs.readFileSync(filePath, "utf8");
+} catch (err) {
+  str =
+    "# 404: Page Not Found\n\nSorry, but the page you were trying to view does not exist.\n\nIt looks like this was the result of either:\n\n- a mistyped address\n- an out-of-date link\n\n[Go back to home page](/blog)";
+}
+``
+blogMap.set("os2024.md", {
+  id: "blog1",
+  title: "Blog 1",
+  content: str,
+});
 
 const page = () => {
   return (
@@ -12,4 +30,5 @@ const page = () => {
   );
 };
 
+export { blogMap };
 export default page;
