c872b19dea64dbcae2ed533cb99da7ab1f49ff81
Harry Yuhao Han <s2162783@ed.ac.uk>
2024-02-04 22:56:57 +0000

    fix slug query path non-catchall bug

--- app/(blog)/blog/[...slug]/page.tsx
+++ app/(blog)/blog/[...slug]/page.tsx
@@ -2,8 +2,13 @@ import MarkdownBlog from "@/app/(blog)/components/MarkdownBlog";
 import { fetchPost } from "@/lib/data";
 
 export default async function Page({ params }: { params: { slug: string } }) {
+  var querypath = "";
+  for (let i = 0; i < params.slug.length -1; i++) {
+    querypath += params.slug[i] + "/";
+  }
+  querypath += params.slug[params.slug.length - 1];
 
-  const res = await fetchPost(params.slug) || "404";
+  const res = await fetchPost(querypath) || "404";
 
   return <MarkdownBlog content={res} />;
 }
