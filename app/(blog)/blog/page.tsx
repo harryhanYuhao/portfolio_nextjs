2d210f51905285d4dec7a2d170942a868f87d61f
Harry Yuhao Han <s2162783@ed.ac.uk>
2024-02-04 13:27:25 +0000

    backend production ready

--- app/(blog)/blog/page.tsx
+++ app/(blog)/blog/page.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 
-const page = () => {
+import { fetchPostList } from "@/lib/data";
+
+async function page() {
+  const res = await fetchPostList();
+  console.log(res);
   return (
     <div className="flex flex-row justify-center ">
       <div className="w-[100vw] text-center">
-        <a href="/blog/dice_uoe_setup.md" className="underline">
-          Dice Set up for OS UoE
-        </a>
+        {res ? (
+          res.map((post) => {
+            return (
+              <div>
+              <a href={`/blog/${post}`} className="underline">{post}</a>
+              </div>);
+          })
+        ) : (
+          <a href="/blog/dice_uoe_setup.md" className="underline">
+            Dice Set up for OS UoE
+          </a>
+        )}
       </div>
     </div>
   );
-};
+}
 
 export default page;
--- lib/data.ts
+++ lib/data.ts
@@ -15,3 +15,22 @@ export async function fetchPost(postName: string) {
     console.error("Error fetching data", error);
   }
 }
+
+export async function fetchPostList() {
+  try {
+    const res = await fetch(
+      `http://${process.env.VIR_IP}:${process.env.DOWNLOAD_SERVER_PORT}/list/`,
+      {
+        method: "GET",
+      },
+    )
+    const data = await res.json();
+    var arr = [];
+    for (let i = 0; i < data.length; i++) {
+      arr.push(data[i]);
+    }
+    return arr;
+  } catch (error) {
+    console.error("Error fetching data", error);
+  }
+}
