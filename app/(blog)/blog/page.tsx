6c5ddcee763f93dc48fc6418338c5a9b6086a36c
Harry Yuhao Han <s2162783@ed.ac.uk>
2024-02-05 22:57:01 +0000

    blog page design

--- app/(blog)/blog/page.tsx
+++ app/(blog)/blog/page.tsx
@@ -1,19 +1,28 @@
 import React from "react";
 
 import { fetchPostList } from "@/lib/data";
+import TextUnderLineAnimation from "@/app/ui/TextUnderLineAnimation";
+
+function StringNicer(str: string) {
+  var tmp = str.charAt(0).toUpperCase() + str.slice(1);
+  return tmp.replace(".md", "").replace(/_/g, " ");
+}
 
 async function page() {
   const res = await fetchPostList();
   console.log(res);
   return (
-    <div className="flex flex-row justify-center ">
-      <div className="w-[100vw] text-center">
+    <div>
+      <div className="w-[100vw] text-center flex flex-col items-center justify-center">
         {res ? (
           res.map((post, key) => {
             return (
-              <div key={key} className="my-2 text-xl">
-              <a href={`/blog/${post}`} className="underline">{post}</a>
-              </div>);
+              <div key={key} className="w-[392px] text-left leading-8 text-lg">
+                <a href={`/blog/${post}`} className="">
+                  <TextUnderLineAnimation text={StringNicer(post)} />
+                </a>
+              </div>
+            );
           })
         ) : (
           <a href="/blog/dice_uoe_setup.md" className="underline">
